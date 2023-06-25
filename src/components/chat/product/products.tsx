526886ff54005ddc1f0fab65a3bfc04f318171d3
Daniel villanueva <danieljx@MacBook-Pro-de-Daniel.local>
2023-06-25 10:05:23 -0500

    Change disable swiperjs for nextjs

--- src/components/chat/product/products.tsx
+++ src/components/chat/product/products.tsx
@@ -2,8 +2,8 @@ import React, { FC } from 'react'
 import { IProduct } from '../../../core/types'
 import Product from './product'
 import Box from '@mui/material/Box'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper'
+// import { Swiper, SwiperSlide } from 'swiper/react' disable for nextjs
+// import { Navigation } from 'swiper' disable for nextjs
 // import 'swiper/scss' disable for nextjs
 
 type Props = {
@@ -11,50 +11,22 @@ type Props = {
 }
 
 const Products: FC<Props> = ({ products }) => {
-  const swiperId = Math.floor(Math.random() * 1000)
-  const swiperPrevButtonClass = `prevEl-${swiperId}`
-  const swiperNextButtonClass = `nextEl-${swiperId}`
+  // const swiperId = Math.floor(Math.random() * 1000)
+  // const swiperPrevButtonClass = `prevEl-${swiperId}`
+  // const swiperNextButtonClass = `nextEl-${swiperId}`
   return (
-    <Swiper
-      spaceBetween={10}
-      breakpoints={{
-        0: {
-          slidesPerView: 1.5,
-        },
-        530: {
-          slidesPerView: 1.5,
-        },
-        700: {
-          slidesPerView: 2,
-        },
-        850: {
-          slidesPerView: 2.5,
-        },
-        1150: {
-          slidesPerView: 2.5,
-        },
-        1450: {
-          slidesPerView: 3,
-        },
-      }}
-      navigation={{
-        prevEl: '.' + swiperPrevButtonClass,
-        nextEl: '.' + swiperNextButtonClass,
-      }}
-      modules={[Navigation]}
-    >
+    <>
       {products.map((product, i) => (
-        <SwiperSlide key={`slide-${i}-product`} className='slider_card-product'>
-          <Box
-            sx={{
-              padding: '15px 5px',
-            }}
-          >
-            <Product key={`product-${i}`} product={product} />
-          </Box>
-        </SwiperSlide>
+        <Box
+          key={`slide-${i}-product`}
+          sx={{
+            padding: '15px 5px',
+          }}
+        >
+          <Product key={`product-${i}`} product={product} />
+        </Box>
       ))}
-    </Swiper>
+    </>
   )
 }
 
